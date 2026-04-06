573b00a8f9d9c68bcf35c3d778326ab057503895
Daksh Kaushik <daksh.kaushik@trangile.com>
2026-04-06 16:09:33 +0530

    Commit by jitu

--- src/main/java/com/cicd/SpringbootCicdApplication.java
+++ src/main/java/com/cicd/SpringbootCicdApplication.java
@@ -15,6 +15,6 @@ public class SpringbootCicdApplication {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello from AWS EC2 Jitu 🚀";
+        return "Hello from AWS EC2 Jituagain 🚀";
     }	
 }
